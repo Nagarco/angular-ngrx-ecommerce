a2183cc7d8a4e2bba7dc214b1987cec304dc9cd4
o.mahmoud <o.mahmoud@moddakir.com>
2025-12-29 23:32:24 +0200

    init `InputFieldComponent`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { InputFieldComponent } from '@/shared/components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, InputFieldComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ecommerce-app';
+  form = new FormGroup({
+    email: new FormControl(),
+  });
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
