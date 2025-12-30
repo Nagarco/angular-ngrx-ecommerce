bbd5a45783a81d236f89cd621d1b91cfc7d09c77
o.mahmoud <o.mahmoud@moddakir.com>
2025-12-30 15:08:54 +0200

    configure app routes and init login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FeaturesRoutes } from '@/common';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: FeaturesRoutes.Auth.path,
+    canActivate: [],
+    loadChildren: () =>
+      import('./features/auth/routes').then((m) => m.AUTH_ROUTES),
+  },
+];
---
+++ src/app/features/auth/routes.ts
@@ -0,0 +1,10 @@
+import { FeaturesRoutes } from '@/common';
+import { Route } from '@angular/router';
+
+export const AUTH_ROUTES: Route[] = [
+  {
+    path: FeaturesRoutes.Login.path,
+    loadComponent: () =>
+      import('./feature-login/login/login.page').then((m) => m.LoginPage),
+  },
+];
