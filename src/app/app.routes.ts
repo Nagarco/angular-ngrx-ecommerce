1736fa145b2f051ffb4ca327fb76904cb0e0ef9f
o.mahmoud <o.mahmoud@moddakir.com>
2025-12-31 14:44:59 +0200

    update routing to use MainLayoutComponent directly instead of lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { FeaturesRoutes } from '@/common';
 import { authGuard, guestGuard } from './core/guards';
+import { MainLayoutComponent } from './shared/components';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,7 @@ export const routes: Routes = [
   {
     path: FeaturesRoutes.Landing.path,
     canActivate: [authGuard],
-    loadComponent: () => import('./shared/components/main-layout/main-layout.component').then((m) => m.MainLayoutComponent),
+    component: MainLayoutComponent,
     loadChildren: () =>
       import('./features/landing/routes').then((m) => m.LANDING_ROUTES),
   },
